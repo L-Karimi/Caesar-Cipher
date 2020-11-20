07cdb4e42e51b0477a7103135d32601b4951aeb5
luciahroyalty101 <luciahkei101@gmail.com>
2020-11-21 00:22:05 +0300

    use while loop to execute first statement

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,5 +5,11 @@ public class App {
 
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
-        boolean runCaesar = true;
+        boolean runCaesar = true; while (runCaesar) {
+
+            System.out.println("Welcome to my Caesar Cipher");
+            System.out.println("============================");
+            System.out.println("Enter encrypt to encrypt or decrypt to decrypt or exit to leave the app");
+            String choice = myScanner.nextLine();
+
 }
