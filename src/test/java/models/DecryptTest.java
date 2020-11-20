1a4662f27bdb0fb3405498408343ec5839b85425
luciahroyalty101 <luciahkei101@gmail.com>
2020-11-21 01:54:00 +0300

    add test 1

--- src/test/java/models/DecryptTest.java
+++ src/test/java/models/DecryptTest.java
@@ -1,11 +1,38 @@
 package models;
+import  org.junit.After;
+import  org.junit.Before;
 
 import org.junit.Test;
+
 
 import static org.junit.Assert.*;
 
 public class DecryptTest {
+
     @Test
     public void Decrypt() {
     }
-}
+
+//    @Test
+//    public void getmInputString() {
+//    }
+//
+//    @Test
+//    public void getmImputString() {
+//
+//        }
+//        @Test
+//        public void getmShift () {
+//        }
+//        @Test
+//        public void decrypt () {
+//        }
+//        @Test
+//        public void testGetmInputString () {
+//        }
+//        @Test
+//        public void testDecrypt () {
+//        }
+//    }
+//
+//
