6f2dbdcd70ad57a70a6da230c1dc20378d31fe07
luciahroyalty101 <luciahkei101@gmail.com>
2020-11-21 00:13:29 +0300

    add scanner to get user imput

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,4 +2,8 @@ import models.Decrypt;
 import models.Encrypt;
 
 public class App {
+
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        boolean runCaesar = true;
 }
