61cefb704477d2ceac1a91927bcef4b88a0242fa
luciahroyalty101 <luciahkei101@gmail.com>
2020-11-20 23:07:45 +0300

    add public access class

--- src/main/java/models/Encrypt.java
+++ src/main/java/models/Encrypt.java
@@ -5,5 +5,18 @@ public class Encrypt {
     private String mImputString;
     private int mShift;
 
+    public Encrypt(String mImputString, int mShift) {
+        this.mImputString = mImputString;
+        this.mShift = mShift;
+    }
 
+    public String getmImputString() {
+        return mImputString;
+    }
+
+    public int getmShift() {
+        return mShift;
+    }
 }
+
+
