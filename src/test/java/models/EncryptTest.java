cff9682c8fa0643bc8b3c2c0e4a175db46cc8c54
luciahroyalty101 <luciahkei101@gmail.com>
2020-11-21 02:13:59 +0300

    add test

--- src/test/java/models/EncryptTest.java
+++ src/test/java/models/EncryptTest.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.*;
 
 public class EncryptTest {
     @Test
-    public void Encrypt() {
+    public void runEncrypt_ifInputIsString() {
+        Encrypt testEncrypt = new Encryptt("abc",3);
+        assertEquals("abc", testEncrypt.getmInputString());
     }
 }
