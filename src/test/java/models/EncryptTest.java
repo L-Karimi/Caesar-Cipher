88df8397c3f3278d7956c510f62f18f0f753d968
luciahroyalty101 <luciahkei101@gmail.com>
2020-11-24 21:45:46 +0300

    modify readme.md,DecryptTest.java and EncryptTest.java

--- src/test/java/models/EncryptTest.java
+++ src/test/java/models/EncryptTest.java
@@ -18,8 +18,10 @@ public class EncryptTest {
         Encrypt testEncrypt = new Encrypt("abc",3);
         assertEquals(3, testEncrypt.getmShift());
     }
-    @Test
-    public void encrypt(){
 
+    @Test
+    public void runEncrypt_ifEncryptsVariousInputs() {
+        Encrypt testEncrypt = new Encrypt("!LAZY BROWN FOX JUMPS OVER A LAZY DOG2", 3);
+        assertEquals("!OX]\\ YURZQ ]R[ MXPSV RY\\U X OX]\\ [R^2",Encrypt.encrypt(testEncrypt));
     }
 }
